0ca986122e5293ac2ca816cae65ceff49aa5d7cd
mrosenback <mathias.rosenback@ha.ax>
2022-03-04 16:11:32 +0200

    Changed name for test cases and completed testNegativeNumberException()

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -6,15 +6,10 @@ public class StringCalculator {
         int result = 0;
 
         if(numbers.length() > 0) {
-            result = Arrays.stream(numbers.split("[,\n/;]")).map(Integer::parseInt).reduce(0, Integer::sum);
-        }
-
-        try {
-            if(result < 0) {
-                throw new IllegalArgumentException("Negatives not allowed " + result);
+            if(numbers.contains("-")) {
+                throw new IllegalArgumentException("Negatives not allowed");
             }
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+            result = Arrays.stream(numbers.split("[,\n/;]")).map(Integer::parseInt).reduce(0, Integer::sum);
         }
 
         return result;
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -21,23 +21,22 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void testAddStringBigNumber() {
+    public void testAddStringUnknownNumber() {
         assertEquals(150, calculator.Add("10,20,30,40,50"));
     }
 
     @Test
-    public void testSplitNewLine() {
+    public void testAddHandlesNewlineSeparator() {
         assertEquals(6, calculator.Add("1\n2,3"));
     }
 
     @Test
-    public void testSplitDelimiters() {
+    public void testAddHandlesDelimiters() {
         assertEquals(3, calculator.Add("//;\n1;2"));
     }
 
     @Test
-    public void testNegative() {
-        Throwable exception = assertThrows(IllegalArgumentException.class, () -> calculator.Add("-5,2"));
-        assertEquals("Negatives not allowed -3", exception.getMessage());
+    public void testNegativeNumberException() {
+        assertThrows(IllegalArgumentException.class, () -> calculator.Add("-5,2"));
     }
 }
